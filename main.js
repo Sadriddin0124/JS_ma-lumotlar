f9cd59f2a53a64f30c7011a5e35939c07885c454
Your Name <you@example.com>
2023-11-21 01:23:32 +0300

    change5

--- main.js
+++ main.js
@@ -1,34 +1,13 @@
-// let obj = {age: 34}
-
-// obj.__proto__.SayName = function(name){
-//         console.log(name)
-//     }
-//     // Bu setter deyiladi ya'ni prototypega yangi property qo'shish
+let menu__link1 = document.getElementById('menu__link')
+let menu = document.getElementById('menu')
+const Click = () => {
+    menu.classList.add('navbar__menu1')
     
     
-//     obj.__proto__.SayName('Sadriddin')
-//     //Bu getter deyiladi
-    
-    
-// let obj = {age: 34}
-// let obj2 = {}
-// obj.__proto__.SayName = function(name){
-//         console.log(name)
-//     }
+}
 
-// obj2.__proto__.SayName('Sadriddin')
-
-// //BU holatda biz birinchi Objectga yangi prototype qo'shsak
-// //faqat bitta object uchun emas hamma Object uchun qo'shiladi
-// //Biz xohlagancha ishlatsak bo'ladi
-
-
-// Biz faqat funksiya emas xohlagan ma'lumot turini qo'sha olamiz va prototypeni ko'ra olamiz
-
-let str = new String('Sadriddin')
-console.log(str) //BU stringni prototypeni ko'rsatib beradi
-// biz bunga number ha qo'shishimiz mumkin
-
-str.__proto__.num = 32
-
-console.log(str.__proto__.num) // 32 chiqadi
+const Remove = () => {
+    let menu = document.getElementById('menu')
+    menu.classList.add('menu')
+    menu.classList.remove('navbar__menu1')
+}
